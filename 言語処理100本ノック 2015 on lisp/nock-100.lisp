1a046ef05c8a9ab07e43248a771b4141c271c328
p-baleine <p.baleine@gmail.com>
2016-05-04 17:41:47 +0900

    ch1

--- 言語処理100本ノック 2015 on lisp/nock-100.lisp
+++ 言語処理100本ノック 2015 on lisp/nock-100.lisp
@@ -10,3 +10,7 @@
 
 (defun preprocess (sentence)
   (map ^(re-replace #/[\.,?]/ % "") (split #\Space sentence)))
+
+(defun seqrnd (seq)
+  "Randomize the elements of a sequence. Destructive on SEQ."
+  (sort seq #'> :key (lambda (x) (random 1.0))))
